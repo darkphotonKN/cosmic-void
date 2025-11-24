ae120a6a88bda546727231c33c1a9f691414cc74
Kranti <darkphoton20@gmail.com>
2025-11-24 21:17:36 +0800

    feat: more ECS structuring

--- game-server/game-service/internal/ecs/entity.go
+++ game-server/game-service/internal/ecs/entity.go
@@ -21,13 +21,23 @@ type ComponentType string
 
 const (
 	ComponentTypePlayer    ComponentType = "Player"
+	ComponentTypeItem      ComponentType = "Item"
 	ComponentTypeTransform ComponentType = "Transform"
+	ComponentTypeVelocity  ComponentType = "Velocity"
 )
 
 type Entity struct {
 	ID         uuid.UUID
 	components map[ComponentType]Component
 	mu         sync.RWMutex
+}
+
+func NewEntity() *Entity {
+	return &Entity{
+		ID:         uuid.New(),
+		components: make(map[ComponentType]Component, 0),
+	}
+
 }
 
 func (e *Entity) AddComponent(component Component) {
@@ -37,6 +47,22 @@ func (e *Entity) AddComponent(component Component) {
 	componentType := component.Type()
 
 	e.components[componentType] = component
+}
+
+func (e *Entity) RemoveComponent(componentType ComponentType) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
+	delete(e.components, componentType)
+}
+
+func (e *Entity) HasComponent(componentType ComponentType) bool {
+	e.mu.RLock()
+	defer e.mu.RUnlock()
+
+	_, exists := e.components[componentType]
+
+	return exists
 }
 
 func (e *Entity) GetComponent(componentType ComponentType) (Component, bool) {
