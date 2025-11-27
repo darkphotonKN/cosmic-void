44da4dbf279a242bea4a1f271c94ec3a858203b0
Kranti <darkphoton20@gmail.com>
2025-11-27 20:42:00 +0800

    feat: updating velocity component to conform expectation from movement system

--- game-server/game-service/internal/components/velocity.go
+++ game-server/game-service/internal/components/velocity.go
@@ -3,15 +3,15 @@ package components
 import "github.com/darkphotonKN/cosmic-void-server/game-service/internal/ecs"
 
 type VelocityComponent struct {
-	VX      float64
-	VY      float64
-	SpeedV0 float64
+	VX    float64
+	VY    float64
+	Speed float64
 }
 
 func (v *VelocityComponent) Type() ecs.ComponentType {
 	return ecs.ComponentTypeVelocity
 }
 
-func NewVelocityComponent(VX, VY, SpeedV0 float64) *VelocityComponent {
-	return &VelocityComponent{VX: VX, VY: VY, SpeedV0: SpeedV0}
+func NewVelocityComponent(VX, VY, Speed float64) *VelocityComponent {
+	return &VelocityComponent{VX: VX, VY: VY, Speed: Speed}
 }
