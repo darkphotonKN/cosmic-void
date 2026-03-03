fa5bcec27e4c572f6de20ac32709c218fec3bb70
kiki ciou <kiki.ciou@cloud-interactive.com>
2026-03-03 21:02:12 +0800

    refactor:update interacion

--- game-server/game-service/internal/systems/interaction.go
+++ game-server/game-service/internal/systems/interaction.go
@@ -1,11 +1,20 @@
 package systems
 
 import (
-	"math"
+	"sync"
 
 	"github.com/darkphotonKN/cosmic-void-server/game-service/internal/components"
 	"github.com/darkphotonKN/cosmic-void-server/game-service/internal/ecs"
 )
+
+// Pool for interactable entities slice
+var interactableEntitiesPool = sync.Pool{
+	New: func() interface{} {
+		// Pre-allocate slice for typical number of interactable objects (doors, chests, etc.)
+		slice := make([]*ecs.Entity, 0, 16)
+		return &slice
+	},
+}
 
 type InteractionSystem struct{}
 
@@ -14,47 +23,66 @@ func NewInterationSystem() *InteractionSystem {
 }
 
 func (s *InteractionSystem) Update(entities []*ecs.Entity) {
+	// Get interactable entities slice from pool
+	interactablesPtr := interactableEntitiesPool.Get().(*[]*ecs.Entity)
+	interactables := *interactablesPtr
+
+	// Always return to pool when done
+	defer func() {
+		// Clear slice before returning to pool
+		for i := range interactables {
+			interactables[i] = nil
+		}
+		interactables = interactables[:0]
+		*interactablesPtr = interactables
+		interactableEntitiesPool.Put(interactablesPtr)
+	}()
+
+	// Step 1: Collect all interactable entities once (avoid nested loop)
 	for _, entity := range entities {
+		_, hasInteractable := entity.GetComponent(ecs.ComponentTypeInteractable)
+		_, hasOpenable := entity.GetComponent(ecs.ComponentTypeOpenable)
+		_, hasTransform := entity.GetComponent(ecs.ComponentTypeTransform)
+
+		if hasInteractable && hasOpenable && hasTransform {
+			interactables = append(interactables, entity)
+		}
+	}
+
+	// Step 2: For each player, check against collected interactables
+	for _, playerEntity := range entities {
 		// validation for player
-		_, hasPlayer := entity.GetComponent(ecs.ComponentTypePlayer)
-		transformComp, hasTransform := entity.GetComponent(ecs.ComponentTypeTransform)
+		_, hasPlayer := playerEntity.GetComponent(ecs.ComponentTypePlayer)
+		transformComp, hasTransform := playerEntity.GetComponent(ecs.ComponentTypeTransform)
 
 		if !hasTransform || !hasPlayer {
 			continue
 		}
 
-		transform := transformComp.(*components.TransformComponent)
+		playerTransform := transformComp.(*components.TransformComponent)
 
-		// --- check door within range ---
-
-		for _, entity := range entities {
-			interactableComp, hasInteractable := entity.GetComponent(ecs.ComponentTypeInteractable)
-			openableComp, hasOpenable := entity.GetComponent(ecs.ComponentTypeOpenable)
-			doorTransformComp, hasTransform := entity.GetComponent(ecs.ComponentTypeTransform)
-
-			if !hasInteractable || !hasOpenable || !hasTransform {
-				continue
-			}
+		// Check each interactable object
+		for _, interactableEntity := range interactables {
+			interactableComp, _ := interactableEntity.GetComponent(ecs.ComponentTypeInteractable)
+			openableComp, _ := interactableEntity.GetComponent(ecs.ComponentTypeOpenable)
+			objectTransformComp, _ := interactableEntity.GetComponent(ecs.ComponentTypeTransform)
 
 			interactable := interactableComp.(*components.InteractableComponent)
 			openable := openableComp.(*components.OpenableComponent)
-			doorTransform := doorTransformComp.(*components.TransformComponent)
+			objectTransform := objectTransformComp.(*components.TransformComponent)
 
-			// area in a circle around point from main entity (player)
+			// Calculate distance using squared distance (avoid sqrt for performance)
+			dx := playerTransform.X - objectTransform.X
+			dy := playerTransform.Y - objectTransform.Y
+			distanceSquared := dx*dx + dy*dy
+			rangeSquared := interactable.Range * interactable.Range
 
-			// calculate range via range provided by interactable
-			xDiff := math.Pow(transform.X-doorTransform.X, 2)
-			yDiff := math.Pow(transform.Y-doorTransform.Y, 2)
-			distanceBetween := math.Sqrt(xDiff + yDiff)
-
-			// too far
-			if distanceBetween > interactable.Range {
-				continue
+			// Check if within interaction range
+			if distanceSquared <= rangeSquared {
+				// object close enough to be interacted
+				// flip object's openable state to open or closed
+				openable.IsOpen = !openable.IsOpen
 			}
-
-			// object close enough to be interacted
-			// flip object's openable state to open or closed
-			openable.IsOpen = !openable.IsOpen
 		}
 	}
 }
