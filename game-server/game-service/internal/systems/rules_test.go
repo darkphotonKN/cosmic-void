d9d220529cf4801d9b9e737c2e0a0b4dc711bbe3
Kranti <darkphoton20@gmail.com>
2026-03-08 19:05:29 +0800

    test: updating tests for rule system.

--- game-server/game-service/internal/systems/rules_test.go
+++ game-server/game-service/internal/systems/rules_test.go
@@ -1,7 +1,9 @@
 package systems_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/darkphotonKN/cosmic-void-server/game-service/internal/components"
 	"github.com/darkphotonKN/cosmic-void-server/game-service/internal/ecs"
@@ -9,26 +11,17 @@ import (
 	"github.com/darkphotonKN/cosmic-void-server/game-service/internal/systems"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 // TestRulesSystem_Update_EndsGameWhenOnePlayerLeft tests that the game ends
 // when only one player is left alive
 func TestRulesSystem_Update_EndsGameWhenOnePlayerLeft(t *testing.T) {
-	// Setup
 	rulesSystem := systems.NewRulesSystem()
 	endSessionCh := make(chan bool, 1)
 	deltaTime := 0.016 // 60 FPS frame time
 
 	// Create entity manager and entities
 	em := ecs.NewEntityManager()
-
-	// test entities
-
-	// TODO: Set up match progress component
-	// TODO: Set some players as eliminated
-
-	entities := em.GetAllEntities()
 
 	players := []struct {
 		MemberID uuid.UUID
@@ -51,16 +44,51 @@ func TestRulesSystem_Update_EndsGameWhenOnePlayerLeft(t *testing.T) {
 		})
 	}
 
+	entities := em.GetAllEntities()
+
+	// game not ended after this call, players are still alive
 	rulesSystem.Update(deltaTime, entities, endSessionCh)
 
-	// no eliminations yet at this point, game shouldn't end
+	select {
+	case endSession := <-endSessionCh:
+		t.Fatalf("should not have gotten end session, but got %v", endSession)
+	default:
+	}
 
-	for endSession := range endSessionCh {
-		assert.Equal(t, true, endSession)
-		if endSession {
-			break
+	for idx, entity := range entities {
+		if idx == 0 {
+			// grab first player
+
+			// validate in case test entity changes
+			// we want health component specifically
+			isPlayer := entity.HasComponent(ecs.ComponentTypePlayer)
+			if !isPlayer {
+				t.Fatal("Could not get player entity from pool of entities to test on.")
+				return
+			}
+
+			healthComp, exists := entity.GetComponent(ecs.ComponentTypeHealth)
+
+			if !exists {
+				t.Fatal("Could not get player entity's health component to test on.")
+				return
+			}
+
+			// eliminate player
+			healthComp.(*components.HealthComponent).IsEliminated = true
 		}
 	}
+
+	// game ended after this, only one player left
+	rulesSystem.Update(deltaTime, entities, endSessionCh)
+
+	select {
+	case endSession := <-endSessionCh:
+		assert.Equal(t, true, endSession)
+	case <-time.After(time.Second):
+		t.Fatal("Didnt receive end game session before timeout.")
+	}
+
 }
 
 // TestRulesSystem_Update_ContinuesWhenMultipleAlive tests that the game continues
